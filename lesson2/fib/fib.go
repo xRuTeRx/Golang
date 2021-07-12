17f42089219311b4aba7f88eb3f2c1e41e704d3e
xruterx <78423475+xRuTeRx@users.noreply.github.com>
2021-07-12 13:03:44 +0300

    in one line

--- lesson2/fib/fib.go
+++ lesson2/fib/fib.go
@@ -4,13 +4,11 @@ import "fmt"
 
 func OutFibanacciLine(x int) {
 	fmt.Print("1 1")
-	var (
-		a, b, c int = 1, 1, 2
-	)
+	var a, b, c int = 1, 1, 2
+
 	for i := 3; i <= x; i++ {
 		c = a + b
-		a = b
-		b = c
+		a, b = b, c
 		fmt.Print(" ", c)
 	}
 	fmt.Println("")
